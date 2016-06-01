aa2162804d937c95fd812b267fdad51a95406483
jcdourado <jcd17071997@hotmail.com>
2016-06-01 12:11:06 -0300

    Classes mapeadas no hibernate

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -1,46 +1,61 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+@Entity
 public class Conta {
 	private int id;
 	private int frequencia;
 	private String nome;
 	private String descricao;
 	private float valor;
-	private String usuario;
+	private Usuario usuario;
+	@Id
+	@GeneratedValue
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+	@Column
 	public int getFrequencia() {
 		return frequencia;
 	}
 	public void setFrequencia(int frequencia) {
 		this.frequencia = frequencia;
 	}
+	@Column
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	@Column
 	public String getDescricao() {
 		return descricao;
 	}
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	@Column
 	public float getValor() {
 		return valor;
 	}
 	public void setValor(float valor) {
 		this.valor = valor;
 	}
-	public String getUsuario() {
+	@ManyToOne
+	@JoinColumn(name="usuario",nullable=false)
+	public Usuario getUsuario() {
 		return usuario;
 	}
-	public void setUsuario(String usuario) {
+	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
 }
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,7 +1,12 @@
 package model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 
 import java.util.List;
 
+@Entity
 public class Usuario {
 	private String nome;
 	private String usuario;
@@ -10,42 +15,51 @@ public class Usuario {
 	private boolean logado;
 	private List<Conta> contas;
 	private List<Recebimento> recebimentos;
+	
+	@Column
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	@Id
 	public String getUsuario() {
 		return usuario;
 	}
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
+	@Column(unique=true)
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	@Column
 	public String getSenha() {
 		return senha;
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+	@Transient
 	public boolean isLogado() {
 		return logado;
 	}
 	public void setLogado(boolean logado) {
 		this.logado = logado;
 	}
+	@Transient
 	public List<Conta> getContas() {
 		return contas;
 	}
 	public void setContas(List<Conta> contas) {
 		this.contas = contas;
 	}
+	@Transient
 	public List<Recebimento> getRecebimentos() {
 		return recebimentos;
 	}
